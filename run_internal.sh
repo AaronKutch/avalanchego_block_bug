462557cc2d89008d2979cfa2fa4aa30663649d96
Aaron Kutch <aaronkutch@att.net>
2022-07-06 19:51:27 -0500

    exhibit 0 eth_gasPrice behavior

--- run_internal.sh
+++ run_internal.sh
@@ -1,8 +1,8 @@
 
 avalanchego \
     --genesis="/test_folder/ETHGenesis.json" \
-    --build-dir="/avalanchego/build/" \
     --network-id=15 \
+    --build-dir="/avalanchego/build/" \
     --public-ip=127.0.0.1 \
     --http-port=8545 \
     --db-type=memdb \
@@ -10,20 +10,24 @@ avalanchego \
 
 sleep 10
 
-# check balances that the genesis file worked correctly
-curl -s --header "content-type: application/json" --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0xBf660843528035a5A4921534E156a27e64B231fE\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
-curl -s --header "content-type: application/json" --data "{\"id\":4,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0xb3d82b1367D362DE99aB59A658165AFf520CBd4d\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
-curl -s --header "content-type: application/json" --data "{\"id\":7,\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionCount\",\"params\":[\"0xBf660843528035a5A4921534E156a27e64B231fE\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[]}" http://localhost:8545/ext/bc/C/rpc
 
-# transaction for sending from the 0xBf... address to 0xb3...
-curl -s --header "content-type: application/json" --data "{\"id\":8,\"jsonrpc\":\"2.0\",\"method\":\"eth_sendRawTransaction\",\"params\":[\"0xf872808506fc23ac00825dc094b3d82b1367d362de99ab59a658165aff520cbd4d8b084595161401484a00000080830150f4a03dedf66bb8140cbbdb10dae14f3016711683906bd0e98d538417c429c930149ea003d7802d60260fc12ba470dc4a578084bfbef62b9aa960ad9a96827471af9844\"]}" http://localhost:8545/ext/bc/C/rpc
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_sendRawTransaction\",\"params\":[\"0xf86480854abd174a00825dc094798d4ba9baf0064ec19eb4f0a1a45785ae9d6dfc018041a0c6431389b47c22e22c7fafacd2f387643be587f3f147b314f6c15fd287868350a0153e20d10c482f80fa35ff3f631864730e475a04c509c412ee18a04b99251500\"]}" http://localhost:8545/ext/bc/C/rpc
 
-sleep 20
+sleep 2
 
-# check for changes
-curl -s --header "content-type: application/json" --data "{\"id\":2,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0xBf660843528035a5A4921534E156a27e64B231fE\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
-curl -s --header "content-type: application/json" --data "{\"id\":4,\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0xb3d82b1367D362DE99aB59A658165AFf520CBd4d\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
-curl -s --header "content-type: application/json" --data "{\"id\":7,\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionCount\",\"params\":[\"0xBf660843528035a5A4921534E156a27e64B231fE\",\"latest\"]}" http://localhost:8545/ext/bc/C/rpc
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[]}" http://localhost:8545/ext/bc/C/rpc
 
-# blocks are not being produced, the avalanche logs also attest to this
-curl -s --header "content-type: application/json" --data "{\"id\":15082,\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[]}" http://localhost:8545/ext/bc/C/rpc
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_sendRawTransaction\",\"params\":[\"0xf86401854abd174a00825dc094798d4ba9baf0064ec19eb4f0a1a45785ae9d6dfc018041a0b73c2e801dc4f48c5018290642acd88317191b85c033a2adf329260e9ee638faa055cc345bc16b1a7432b27eb33230af58a5611f8eaa2a9087bc02b0ee3deadcbd\"]}" http://localhost:8545/ext/bc/C/rpc
+
+sleep 2
+
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[]}" http://localhost:8545/ext/bc/C/rpc
+
+sleep 2
+
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_sendRawTransaction\",\"params\":[\"0xf86402854abd174a00825dc094798d4ba9baf0064ec19eb4f0a1a45785ae9d6dfc018041a0061883a180026dc4aee6937bcc08a644bc6f3e64331c5ae97f7956432bdd4503a0722c121ed84074f86d854f67060abe69fadb2e2328bb3815b875a6ac9c09063e\"]}" http://localhost:8545/ext/bc/C/rpc
+
+sleep 2
+
+curl -s --header "content-type: application/json" --data "{\"id\":0,\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[]}" http://localhost:8545/ext/bc/C/rpc
